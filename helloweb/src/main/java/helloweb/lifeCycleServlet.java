3d361fc39427106f879a8b1313f90ca7068e8f5d
user <user@DESKTOP-EKQUFAN>
2022-01-18 10:32:49 +0900

    LifeCycle

---
+++ helloweb/src/main/java/helloweb/lifeCycleServlet.java
@@ -0,0 +1,47 @@
+package helloweb;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class lifeCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("LifeCycleServlet.doGet(...) called");
+	}
+	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 System.out.println("LifeCycleServlet.doPost(...) called");
+		 
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		 System.out.println("LifeCycleServlet.service(...) called");
+		super.service(req, resp);
+	}
+
+
+	@Override
+	public void destroy() {
+		 System.out.println("LifeCycleServlet.destroy(...) called");
+		 super.destroy();
+	}
+
+
+	@Override
+	public void init() throws ServletException {
+		 System.out.println("LifeCycleServlet.init(...) called");
+		 super.init();
+	}
+
+
+
+}
